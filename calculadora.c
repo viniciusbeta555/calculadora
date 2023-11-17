a590c5f901011699c8a9a46123977b8a03c16269
viniciusbeta555 <96143281+viniciusbeta555@users.noreply.github.com>
2023-11-17 15:35:36 -0300

    Add files via upload

--- calculadora.c
+++ calculadora.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include <stdio.h> //BIBLIOTECA PADRÃO DO C: FUNÇÃO DE ENTRADA E SAIDA DE DADOS
 
 int main (){
 
-    int oper, quant, n, x, y;
+    int oper, quant, n;
     int mult = 1;
-    float a, result;
+    float a, result, div1, div2;
 
-    printf("\nEscolha uma operação matematica: \n");
+    printf("\nEscolha uma operação matematica: \n"); // SELECIONA A OPERAÇÃO QUE VAI SER USADA
     printf("Somar = 1\n");
     printf("Subtrair = 2\n");
     printf("Multiplicar = 3\n");
     printf("Dividir = 4\n");
     scanf("%d", &oper);
 
-    if(oper == 1){
-        printf("Quantos valores voce gostaria de somar?\n");
+    if(oper == 1){  // OPÇÃO ESCOLHIDA = SOMA
+        printf("Quantos valores voce gostaria de somar?\n"); // INSERIR A QUANTIDADE QUE DESEJA SOMAR
         scanf("%d",&quant);
 
         for(n = 0; n < quant; n++){
@@ -26,8 +26,8 @@ int main (){
         printf("\nO resultado é: %.2f", result);
     }
 
-    if(oper == 2){
-        printf("Quantos valores voce gostaria de subtrair?\n");
+    if(oper == 2){   // OPÇÃO ESCOLHIDA = SUBTRAÇÃO
+        printf("Quantos valores voce gostaria de subtrair?\n"); // INSERIR A QUANTIDADE QUE DESEJA SOMAR
         scanf("%d",&quant);
 
         for(n = 0; n < quant; n++){
@@ -43,8 +43,8 @@ int main (){
         printf("\nO resultado é: %.2f", result);
     }
 
-    if(oper == 3){
-        printf("Quantos valores voce gostaria de multiplicar?\n");
+    if(oper == 3){  // OPÇÃO ESCOLHIDA = MULTIPLICAÇÃO
+        printf("Quantos valores voce gostaria de multiplicar?\n"); // INSERIR A QUANTIDADE QUE DESEJA SOMAR
         scanf("%d",&quant);
 
         for(n = 0; n < quant; n++){
@@ -56,14 +56,14 @@ int main (){
         printf("\nO resultado é: %d", mult);
     }
 
-    if(oper == 4){ 
+    if(oper == 4){  // OPÇÃO ESCOLHIDA = DIVISÃO
         
-        printf("Digite o primeiro valor: \n");
-        scanf("%d",&x);
-        printf("Digite o segundo valor: \n");
-        scanf("%d",&y);
+        printf("Digite o primeiro valor: \n");  // DIVIDENDO: numero que será dividido
+        scanf("%f",&div1);
+        printf("Digite o segundo valor: \n");  // DIVISOR: o numero que irá dividir o dividendo
+        scanf("%f",&div2);
 
-        result = x/y;
+        result = div1/div2;
 
         printf("\nO resultado é: %.2f", result);
     }
